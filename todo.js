362b5c79cde9093c738982ab131ae3279fc90761
Balaraman-7 <balaramanav1701@gmail.com>
2025-11-09 00:49:33 +0530

    Enhance todo list with task completion feature
    Added a toggleStrike function to mark tasks as completed and updated the add function to include a check button.

--- todo.js
+++ todo.js
@@ -7,11 +7,17 @@ function add()
         alert("Please enter a task");
         return;
     }
-    listitem.innerHTML ='<span class="append-text">' +inputvalue + '</span> <button class="delete-btn" onclick="Delete(event)">Delete</button>';
+    listitem.innerHTML ='<button class="check-btn" onclick="toggleStrike(event)">✅</button><span class="append-text">' +inputvalue + '</span> <button class="delete-btn" onclick="Delete(event)">Delete</button>';
 
     ul.append(listitem);
+    document.getElementById("input-content").value = "";
 }
 function Delete(event)
 {
     event.target.parentElement.remove();
+}
+function toggleStrike(event)
+{
+    let textSpan = event.target.nextElementSibling;
+    textSpan.classList.toggle("strikethrough");
 }
